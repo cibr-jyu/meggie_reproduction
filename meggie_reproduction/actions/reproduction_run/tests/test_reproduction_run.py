926ea012b5c95fd6f3d263d014c767de4db871ca
teekuningas <erkkahe@gmail.com>
2025-03-15 14:39:42 +0200

    Simplify testing

--- meggie_reproduction/actions/reproduction_run/tests/test_reproduction_run.py
+++ meggie_reproduction/actions/reproduction_run/tests/test_reproduction_run.py
@@ -1,6 +1,4 @@
 import os
-import json
-import pkg_resources
 import time
 
 from meggie.utilities.testing import BaseTestAction
@@ -22,13 +20,6 @@ class TestReproductionRunTest(BaseTestAction):
 
         test_dir = os.path.dirname(os.path.abspath(__file__))
         self.actions_path = os.path.join(test_dir, "actions.json")
-
-    def load_action_spec(self, action_name):
-        action_path = pkg_resources.resource_filename("meggie_reproduction", "actions")
-        config_path = os.path.join(action_path, action_name, "configuration.json")
-        with open(config_path, "r") as f:
-            action_spec = json.load(f)
-        return action_spec
 
     def test_reproduction_run(self):
 
